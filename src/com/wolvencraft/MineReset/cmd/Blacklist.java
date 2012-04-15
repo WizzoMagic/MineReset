850c55250d73e449f03401d378cbde1d06235b8c
Kirill Bondarchuk <bondarchuk.kirill@gmail.com>
2012-04-14 20:35:55 -0700

    Inserting placeholders

--- src/com/wolvencraft/MineReset/cmd/Blacklist.java
+++ src/com/wolvencraft/MineReset/cmd/Blacklist.java
@@ -1,5 +1,22 @@
 package com.wolvencraft.MineReset.cmd;
 
-public class Blacklist {
+import org.bukkit.command.CommandSender;
 
+import com.wolvencraft.MineReset.CommandManager;
+import com.wolvencraft.MineReset.MineReset;
+
+public class Blacklist
+{
+	public static void run()
+	{
+		CommandSender sender = CommandManager.getSender();
+		MineReset plugin = CommandManager.getPlugin();
+		
+		// This function should allow the user to enable blacklist / whitelist for each
+		// individual mine, as well as add and remove blocks from them. Checks for the
+		// correctness of arguments should be included.
+		
+		// Help.java includes command help. Util.java has a bunch of helpful functions
+		// for accessing the configuration and printing out messages to the user.
+	}
 }
--- src/com/wolvencraft/MineReset/cmd/Protection.java
+++ src/com/wolvencraft/MineReset/cmd/Protection.java
@@ -5,15 +5,15 @@ import org.bukkit.command.CommandSender;
 import com.wolvencraft.MineReset.CommandManager;
 import com.wolvencraft.MineReset.MineReset;
 
-public class Auto
+public class Protection
 {
 	public static void run()
 	{
 		CommandSender sender = CommandManager.getSender();
 		MineReset plugin = CommandManager.getPlugin();
 		
-		// This function should allow the user to enable the reset timer for each
-		// individual mine, as well as change the reset time from them. Checks for the
+		// This function should allow the user to enable the protection for each
+		// individual mine, as well as edit its properties. Checks for the
 		// correctness of arguments should be included.
 		
 		// Help.java includes command help. Util.java has a bunch of helpful functions
