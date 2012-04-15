d55597c5ee3f15b95ea24f3d7d933ea2dc9a3407
Kirill Bondarchuk <bondarchuk.kirill@gmail.com>
2012-04-15 05:32:35 -0700

    Added accessor and modifier methods for the current mine; useful in Edit.java

--- src/com/wolvencraft/MineReset/CommandManager.java
+++ src/com/wolvencraft/MineReset/CommandManager.java
@@ -7,15 +7,18 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 
-import com.wolvencraft.MineReset.cmd.*;
-
 import com.wolvencraft.MineReset.cmd.Help;
+import com.wolvencraft.MineReset.cmd.Reset;
+import com.wolvencraft.MineReset.cmd.Save;
+import com.wolvencraft.MineReset.cmd.Select;
+import com.wolvencraft.MineReset.cmd.Util;
 
 public class CommandManager implements CommandExecutor
 {
 	private static CommandSender sender;
 	private static MineReset plugin;
 	private static Location[] loc = null;
+	private static String curMine = null;
 	
 	public CommandManager(MineReset plugin)
 	{
@@ -30,7 +33,7 @@ public class CommandManager implements CommandExecutor
 		
 		if(!command.getName().equalsIgnoreCase("mine")) return false;
 		
-		if(args.length == 0) Help.get();
+		if(args.length == 0) Help.run();
 		
 		if(args[0].equalsIgnoreCase("info")) {}
 		else if(args[0].equalsIgnoreCase("list")) {}
@@ -42,7 +45,10 @@ public class CommandManager implements CommandExecutor
 		{
 			Select.run(args);
 		}
-		else if(args[0].equalsIgnoreCase("save")) {}
+		else if(args[0].equalsIgnoreCase("save"))
+		{
+			Save.run(args);
+		}
 		else if(args[0].equalsIgnoreCase("edit")) {}
 		else if(args[0].equalsIgnoreCase("delete")) {}
 		else if(args[0].equalsIgnoreCase("auto")) {}
@@ -90,5 +96,24 @@ public class CommandManager implements CommandExecutor
 	public static Location[] getLocation()
 	{
 		return loc;
+	}
+	
+	/**
+	 * Returns the name of a mine that is being edited
+	 * @return String is a mine is selected, null if it is not
+	 */
+	public static String getMine()
+	{
+		return curMine;
+	}
+	
+	/**
+	 * Sets the name of a current mine to a value specified
+	 * @param newMine The newly selected mine name
+	 */
+	public static void setMine(String newMine)
+	{
+		curMine = newMine;
+		return;
 	}
 }
